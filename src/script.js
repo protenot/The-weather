87e3767ad39380be9b167638d10f81f22632d214
Olga_Belaya <protenot@yandex.ru>
2023-05-11 13:52:43 +0300

    city in browser

--- src/script.js
+++ src/script.js
@@ -26,20 +26,39 @@
       // put your code here
       
     }
-  
+    
     formEl.addEventListener("submit", async (ev) => {
       // чтобы не перезагружать страницу
       ev.preventDefault();
   
       // читаем значение из формы
       const formElement = ev.target;
+     // console.log(formElement);
       const inputEl = formElement.querySelector("input");
+      
       const cityName = inputEl.value;
+      console.log(cityName);
       inputEl.value = "";
   
       const weather = await getWeather(cityName);
-      showWeather(weatherInfoEl, weather);
-    });
-  })();
+      console.log(weather.name);
+      console.log(weather.main.temp);
+    //  showWeather(weatherInfoEl, weather);
   
+ 
+   
+    console.log(weather.main.temp)
   
+  let cityN = document.querySelector('.city-name');
+  cityN.textContent = weather.name;
+  //document.append(city);
+  //console.log(city)
+ /* let temperature =document.createElement("div");
+  temperature.innerText = "Погода:" + weather.main.temp;.
+  document.append(temperature);
+  let avatar = document.createElement("img");
+
+  avatar.src = "http://openweathermap.org/img/wn/04d@2x.png";
+  document.append(avatar);*/
+});
+})();

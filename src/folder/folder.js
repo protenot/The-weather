9e1d3ee5b77dfa99bcf72ac812b193cce8d76c19
Olga_Belaya <protenot@yandex.ru>
2024-08-13 18:57:16 +0300

    feature | add content to folder

---
+++ src/folder/folder.js
@@ -0,0 +1,7 @@
+async function getRecipeCommom(id){
+    const APIKEY = 'cea5775e8e6340d19ba2df6cd5863531';
+    const response2 = await fetch(`https://api.spoonacular.com/recipes/${id}/summary?apiKey=${APIKEY}`)
+  return await response2.json() 
+  
+  }
+  

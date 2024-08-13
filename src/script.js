1f3026cd55921033268201c855c628cbe1908308
Olga_Belaya <protenot@yandex.ru>
2024-08-13 18:45:37 +0300

    project | add trash

--- src/script.js
+++ src/script.js
@@ -1,118 +1,76 @@
-import {meal} from './meal.js'
+//import {meal, idrev} from './meal.js'
 
-(async function () {
-    // Получаем указатели на нужные элементы
-    const formEl = document.querySelector("form");
-    const weatherInfoEl = document.querySelector("#weatherInfo");
-  
-    function showWeather(el, weatherInfo) {
-      el.innerHTML = JSON.stringify(weatherInfo, null, 2);
-    }
-  
-    /**
-     * Функция должна делать запрос на
-     * https://api.openweathermap.org/data/2.5/weather?units=metric&q={{CITY_NAME}}&appid={{APP_ID}}
-     * где
-     *  {{CITY_NAME}} должен быть заменен на имя города
-     *  {{APP_ID}} должен быть заменен на ключ приложения
-     * Запрос возвращает данные в формате JSON
-     *
-     * функция должна возвращать (Promise) данные с информацией о погоде
-     * @param {string} cityName
-     */
-    const API_KEY = 'c768bc4e962d2a69c28ba404045dc96c';
-    async function getWeather(cityName) {
-      
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${API_KEY}`);
-      
-      return await response.json();
-      // put your code here
-      
-    }
+
     async function getRecipe(meal){
       const APIKEY = 'cea5775e8e6340d19ba2df6cd5863531'
       const response1 = await fetch (`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIKEY}&ingredients=${meal}`)
         return await response1.json();
  
     }
-   // const mealResult = getRecipe('lemon').then((data)=>console.log(data))
+    const  mealResult =await getRecipe('lemon')//.then((data)=> {data})// = data console.log(data))
     
- // console.log(mealResult)
- console.log (meal[1])
+  console.log(mealResult[0].id)
+ //console.log (meal[1])
+ const container = document.querySelector('#container');
 
- function getDataFromPromise(mealResult){
+ /* function getDataFromPromise(mealResult){
   console.log(mealResult.length);
   for (let i =0; i < mealResult.length; i++){
 
 
   }
- }
- getDataFromPromise(meal)
- 
- 
- let listOfMeal = document.createElement('div')
+ } */
 
 
-    formEl.addEventListener("submit", async (ev) => {
-      // чтобы не перезагружать страницу
-      ev.preventDefault();
-  
-      // читаем значение из формы
-      const formElement = ev.target;
-     // console.log(formElement);
-      const inputEl = formElement.querySelector("input");
-      
-      const cityName = inputEl.value;
-      console.log(cityName);
-      inputEl.value = "";
-  
-      const weather = await getWeather(cityName);
-      console.log(weather.name);
-      console.log(weather.main.temp);
-    // showWeather(weatherInfoEl, weather);
-  
- 
-   
-    console.log(weather.main.temp)
-  
-  let cityN = document.querySelector('.city-name');
-  cityN.textContent = weather.name;
-  let temperatureData = document.querySelector('.temperature-data');
-  temperatureData.textContent = Math.round(weather.main.temp);
- let iconId =  weather.weather[0].icon;
- let avatar = document.querySelector(".avatar");
- avatar.src = "http://openweathermap.org/img/wn/"
- +iconId+"@2x.png";
- let container = document.querySelector('.container'); 
- container.append(avatar);
- console.log (iconId);
- const button = document.querySelector('button');
- button.addEventListener("click", addParagraph);
- function addParagraph() {
-  const citySaved =  cityN.textContent;
-  //const input = document.querySelector(".text-field");
-  
-    const text = citySaved;
-    getElementToList(text);
-    clearField();
-    return text;
-  
+//let superResult= getDataFromPromise(meal)
+
+
+
+async function getRecipeCommom(id){
+  const APIKEY = 'cea5775e8e6340d19ba2df6cd5863531';
+  const response2 = await fetch(`https://api.spoonacular.com/recipes/${id}/summary?apiKey=${APIKEY}`)
+return await response2.json() 
+
 }
-function getElementToList(text) {
-  const paragraph = document.createElement("p");
-  paragraph.innerText = text;
-  const blockParagraphs = document.querySelector(".container");
-  if (blockParagraphs) {
-    blockParagraphs.append(paragraph);
-  }
-}
- /* let temperature =document.createElement("div");
-  temperature.innerText = "Погода:" + weather.main.temp;.
-  document.append(temperature);
-  let avatar = document.createElement("img");
 
-  avatar.src = "http://openweathermap.org/img/wn/04d@2x.png";
-  document.append(avatar);*/
-});
 
-})();
+let idSR = mealResult[1].id;
+
+ let idNow = await getRecipeCommom(idSR)//.then((res) =>Object.values(res))
+
+console.log(idNow.summary)
+
+
+
+ const listOfMeal = document.createElement('ul');
+ listOfMeal.classList.add('list');
+ //listOfMeal.textContent = meal;
+ console.log(listOfMeal)
+ container.append(listOfMeal)
+
+ let listOfMealItem = document.createElement('li')
+ listOfMealItem.classList.add ('listItem')
+ listOfMeal.append(listOfMealItem)
+
+ let listOfMealImage = document.createElement('img');
+ listOfMealImage.classList.add('listImage')
+ listOfMealItem.append(listOfMealImage);
+ listOfMealImage.src = mealResult[1].image
+
+ let listOfMealTitle=  document.createElement('h2')
+ listOfMealTitle.classList.add('listTitle');
+ listOfMealItem.prepend(listOfMealTitle)
+ listOfMealTitle.textContent = [mealResult[1].title]
+
+ let listOfMealRecipe=  document.createElement('p')
+ listOfMealRecipe.classList.add('listRecipe');
+ listOfMealItem.append(listOfMealRecipe)
+// listOfMealRecipe.textContent = idNow.then(res=>res.summary)
+ listOfMealRecipe.innerHTML= idNow.summary
+ 
+
+
+
+
+
+

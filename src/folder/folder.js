1ecb887c2963368ad614cbb7213ac11554bc0a42
Olga_Belaya <protenot@yandex.ru>
2024-08-14 16:49:44 +0300

    feature | another strange commit

--- src/folder/folder.js
+++ src/folder/folder.js
@@ -4,9 +4,4 @@ async function getRecipeCommom(id){
   return await response2.json() 
   
   }
-  async function getRecipeCommom1(id){
-    const APIKEY = 'cea5775e8e6340d19ba2df6cd5863531';
-    const response2 = await fetch(`https://api.spoonacular.com/recipes/${id}/summary?apiKey=${APIKEY}`)
-  return await response2.json() 
-  
-  }
+  // это просто комментарий 

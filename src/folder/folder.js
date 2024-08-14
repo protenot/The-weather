5c31e520015c933daddff7afffbb2cc5b48ceddd
Olga_Belaya <protenot@yandex.ru>
2024-08-14 15:36:21 +0300

    project | add another indormation in folder

--- src/folder/folder.js
+++ src/folder/folder.js
@@ -4,14 +4,4 @@ async function getRecipeCommom(id){
   return await response2.json() 
   
   }
-<<<<<<< HEAD
-  async function getRecipeCommom1(id){
-    const APIKEY = 'cea5775e8e6340d19ba2df6cd5863531';
-    const response2 = await fetch(`https://api.spoonacular.com/recipes/${id}/summary?apiKey=${APIKEY}`)
-  return await response2.json() 
-  
-  }
-  
-=======
   // это просто комментарий 
->>>>>>> 3b1e997 (project | add another indormation in folder)
